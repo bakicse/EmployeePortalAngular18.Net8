711366a0dc8e77718324f4965496e3f433826549
Mohammad Jannatul Baki <jannatul14.baki@gmail.com>
2024-09-11 12:15:57 +0600

    2nd commit

---
+++ EmployeePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository emp;
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+                this.emp = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allEmployees = await emp.GetEmployees();
+            return Ok(allEmployees);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee(Employee vm)
+        {
+            await emp.SaveEmployee(vm);
+            return Ok(vm);
+        }
+    }
+}
---
+++ EmployeePortal/Migrations/20240911051246_Initial setup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeePortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
