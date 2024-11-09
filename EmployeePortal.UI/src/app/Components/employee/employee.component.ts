aa51e55dfcb7d8f661a850b9215de1f22031e791
Mohammad Jannatul Baki <jannatul14.baki@gmail.com>
2024-11-09 17:25:06 +0600

    see save modal in console

--- EmployeePortal.UI/src/app/Components/employee/employee.component.ts
+++ EmployeePortal.UI/src/app/Components/employee/employee.component.ts
@@ -1,20 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+@ViewChild('myModal') model: ElementRef | undefined;
 
-  openModel()
+  employeeform: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder){}
+  ngOnInit(): void {
+    this.setFormState();
+  }
+  openModal() 
   {
-      const empModel = document.getElementById('myModal');
-      if(empModel != null)
+      const empModal = document.getElementById('myModal');
+      if(empModal != null)
       {
-        empModel.style.display = 'block'
+        empModal.style.display = 'block'
       }
+  }
+  closeModal(){
+    if(this.model != null){
+        this.model.nativeElement.style.display = 'none';
+    }
+  }
+  onSubmit(){
+    console.log(this.employeeform.value);
+  }
+  setFormState(){
+    this.employeeform = this.fb.group({
+      id: [0],
+      name: ['',[Validators.required]],
+      email: ['',[Validators.required]],
+      mobile: ['',[Validators.required]],
+      age: ['',[Validators.required]],
+      salary: ['',[Validators.required]],
+      status: [false,[Validators.required]],
+    });
   }
 }
